dc656374ef4d1fddbea05f13bf67aba6432a15ee
Richa Roy <richaroydps@gmail.com>
2022-05-03 23:56:45 +0530

    new commit

--- main.js
+++ main.js
@@ -1,40 +1,26 @@
-
-  document.querySelector("#input").addEventListener("keydown", (event) => {
-    if (event.key == "Enter")
-      apiRequest();
-  });
-
-  document.querySelector("#search").addEventListener("click", () => {
-    apiRequest();
-  });
-var inputvalue=document.querySelector('#input');
-  apiRequest = () => {
-
-
-    const url = 'https://api.harvardartmuseums.org/exhibition?venue=HAM'+inputvalue.value+'&per_page=4&apikey=6a03fca0-2901-45e9-8f6c-855b859a67ad';
-
+var imgsrc = ['https://nrs.harvard.edu/urn-3:HUAM:DDC252445_dynmc?height=675', 'https://nrs.harvard.edu/urn-3:HUAM:50737_dynmc?height=675','https://nrs.harvard.edu/urn-3:HUAM:51267_dynmc?height=675','https://nrs.harvard.edu/urn-3:HUAM:DDC232089_dynmc?height=675','https://nrs.harvard.edu/urn-3:HUAM:75699_dynmc?height=675','https://nrs.harvard.edu/urn-3:HUAM:INV121896_dynmc?height=675','https://nrs.harvard.edu/urn-3:HUAM:50736_dynmc?height=675','https://nrs.harvard.edu/urn-3:HUAM:76149_dynmc?height=675','https://nrs.harvard.edu/urn-3:HUAM:51253_dynmc?height=675','https://nrs.harvard.edu/urn-3:HUAM:INV054851_dynmc?height=675']
+var url = 'https://api.harvardartmuseums.org/exhibition?apikey=6a03fca0-2901-45e9-8f6c-855b859a67ad';
     fetch(url)
-
-      .then(response => {
-        if (!response.ok) throw Error(response.statusText);
-        return response.json();
+      .then((data) => {
+        return data.json();
       })
-
-      .then(data => {
-        loadImages(data);
-        console.log(data);
+      .then((apidata) => {
+          console.log(apidata);
+          document.getElementById("searchItem").addEventListener("click",()=>{
+            for(var j = 0;j < apidata.records.length;j++){
+              if(document.getElementById('search').value == apidata.records[j].title){
+                console.log("match ka pata nahi kya hua")
+                console.log(j)
+                document.getElementById("apiImage").src = imgsrc[j]
+                break
+                }
+                else if(document.getElementById('search').value != apidata.records[j].title){
+                  // console.log("")
+                  continue
+                }
+              }
+          })
       })
-      .catch(error => console.log(error));
-  }
-
-    loadImages = (data) => {
-    for (let i = 0; i < data.results.length; i++) {
-      let image = document.createElement("div");
-      image.className = "img";
-      image.style.backgroundImage = "url(" + data.results[i].urls.raw + "&w=1366&h=768" + ")";
-      image.addEventListener("dblclick", function () {
-        window.open(data.results[i].links.download, '_blank');
-      })
-      document.querySelector("#grid").appendChild(image);
-    }
-  }
+      .catch((err) => {
+        console.log(err);
+      });
